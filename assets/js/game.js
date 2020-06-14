1825bddaf245d4637807b2067040bde9a9c6495b
alex donnelly <a.james.donnelly@gmail.com>
2020-06-14 16:38:42 -0500

    player name upgrades

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,7 +78,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
         // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-  
+
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyObj = enemyInfo[i];
   
@@ -169,9 +169,23 @@ var shop = function() {
         return value;
       };
     
-      //playerName function expression. this becomes a player object. 
+    // function to set name
+    var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    
+    return name;
+  };
+
+      // player object. 
+
       var playerInfo = {
-        name: window.prompt("What is your robot's name?"),
+        name: getPlayerName(),
         health: 100,
         attack: 10,
         money: 10,
